9c66a19428671e1517d2e8af78e92bd5c7c9802f
imshcheglov <Ivan_mix@mail.ru>
2024-03-26 22:14:48 +0500

    fix

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,13 @@
 
 public class CreditPaymentService {
 
+    /**
+     *
+     * @param amountOfCredit сумма кредита
+     * @param interestRate процентная ставка
+     * @param creditTerm срок кредита
+     * @return расчет ежемесячного платежа
+     */
     public int calculate(int amountOfCredit, double interestRate, int creditTerm) {
         double loanRatePerMonth = (interestRate / 12) / 100;
         double annuityCoefficient = (loanRatePerMonth * Math.pow(1 + loanRatePerMonth, creditTerm) /
